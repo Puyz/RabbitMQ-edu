3a8351daecac333a5d5ae86279c196664649e71e
Ömer temel <42616536+Puyz@users.noreply.github.com>
2024-08-20 14:49:34 +0300

    basic operation

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+// Create Connection
+ConnectionFactory factory = new()
+{
+    Uri = new("")
+};
+
+
+// Connection Activate and Open Channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+// Create Queue 
+/*
+    publisher'daki ile birebir aynı yapılandırmada tanımlanmalıdır.
+*/
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+// Read Message From Queue
+/*
+    autoAck: kuyruktan mesaj okunduğunda silinip silinmemesi
+*/
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+
+consumer.Received += (sender, args) =>
+{
+    // kuyruğa gelen mesajın işlendiği yerdir.
+    // args.Body: kuyruktaki mesajın verisini bütünsel olarak getirir.
+    // args.Body.Span veya args.Body.ToArray(): kuyruktaki mesajın byte verisini getirir.
+    Console.WriteLine(Encoding.UTF8.GetString(args.Body.Span));
+};
+
+Console.Read();
---
+++ Publisher/Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+// Create Connection
+ConnectionFactory factory = new()
+{
+    Uri = new("")
+};
+
+
+// Connection Activate and Open Channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+// Create Queue 
+/*
+    exclusive: eğer true olursa başka bağlantı ile bu kuyruğa (başka channel'dan) ulaşamayız. Consumer ile tüketeceğimiz için false olması gerekiyor.
+*/
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+// Send Message to Queue
+/*
+    RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. 
+    
+    BasicPublish: 
+    * exchange belirtmezsek default olarak direct seçili olur.
+    * direct exchange'de routing key, kuyruk ismine denk geliyor.
+*/
+
+for (int i = 0; i < 120; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("test message "+ i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+
+Console.Read();
